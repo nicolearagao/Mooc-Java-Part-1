6057e334a9fc1b4897d2467fbdf91f72616987f3
Nicole Aragao <63803651+nicolearagao@users.noreply.github.com>
2020-08-25 11:25:37 -0700

    Add files via upload

---
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -0,0 +1,17 @@
+
+public class YourFirstBankTransfer {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // write your program here
+        Account MatthewsAccount = new Account("Matthews account", 1000.0);
+        Account myAccount = new Account("My account", 0);
+
+        MatthewsAccount.withdrawal(100);
+        myAccount.deposit(100);
+
+        System.out.println(MatthewsAccount);
+        System.out.println(myAccount);
+
+    }
+}
