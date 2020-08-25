ea025641a0f4ec45a4b10aa40bb00fc1c410fffa
Nicole Aragao <63803651+nicolearagao@users.noreply.github.com>
2020-08-25 00:52:57 -0700

    Add files via upload

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,13 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account conta1 = new Account ("Nicole`s account", 100.0);
+        System.out.println(conta1);
+        conta1.deposit(20.0);
+        System.out.println(conta1);
+    }
+}
